6d6adf9e14681fc15bfc6c6d08cf4574846e28ab
LeeMinWoo97 <leemin9601@gmail.com>
2021-06-14 17:46:13 +0900

    Java

--- src/JavaPractice/src/JavaPractice/Practice11.java
+++ src/JavaPractice/src/JavaPractice/Practice11.java
@@ -1,5 +1,11 @@
 package JavaPractice;
 
+import JavaPractice.MessagePractice11;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Practice11 {
 	// 계좌생성 및 입출금
 
@@ -7,9 +13,9 @@ public class Practice11 {
 	private int accountNumber; // 계좌번호
 	private String name; // 예금주 이름
 	private String residentRegistrationNumber; // 주민등록번호
-	private long balance; // 잔고
+	private static long balance; // 잔고
 
-	private int accountIndex = 111111111;
+	private static int accountIndex = 111111111; // 시작 계좌번호
 
 	private Practice11(String residentRegistrationNumber, String name) {
 		this.residentRegistrationNumber = residentRegistrationNumber;
@@ -21,48 +27,65 @@ public class Practice11 {
 
 	private void createAccountNumber() {
 		this.accountNumber = accountIndex;
-		accountIndex =accountIndex+ 111;
-		
-	}
-	
+		accountIndex = accountIndex + 111;
 
-	
-		
+	}
+
 	// 잔액 조회
-		public long getBalance() {
-			return balance;
-		}
-		// 입금
-		public void deposit(long amount) {
-			balance += amount;
-		}
-		// 출금
-		public boolean withdraw(long amount) {
-			if (balance < amount)
-				return false;
-			
-			balance -= amount;
-			return true;
-		}
+	public long getBalance() {
+		return balance;
+	}
+
+	// 입금
+	public static long deposit(long amount) {
+		return balance += amount;
+	}
+
+	// 출금
+	public boolean withdraw(long amount) {
+		if (balance < amount)
+			return false;
+
+		balance -= amount;
+		return true;
+	}
 
 	public static void main(String[] args) {
-		/*
-		 * 실행결과 계좌 123-456789 (예금주 : 홍길동) 잔액 : 10000원 20000원 입금합니다. 잔액 : 30000원 45000원
-		 * 출금합니다. 잔액 -15000원
-		 * 
-		 * 문제점 입금, 출금 금액이 음수인 경우 보완 잔액이 음수가 되는 경우 발생 금액이 출력되는 경우에 10000원 -> 10,000원과 같이
-		 * 변경
-		 * 
-		 * BankAccountManager 다수의 BankAccount 관리
-		 * 
-		 */
-		Practice11 pt1 = new Practice11("993351-1504891","김강건");
-		System.out.print("개설하신 계좌의 계좌번호는 :"+pt1.accountNumber+"\n 에금주 :"+pt1.name+"\n 잔액 : "+pt1.balance+ "원 입니다.\n");
-		System.out.print("잔액 :"+pt1.getBalance()+"\n");
-		System.out.print(pt1.deposit(1000));
-		
-		Practice11 pt2 = new Practice11("903351-1506511","박종석");
-		System.out.print("개설하신 계좌의 계좌번호는 :"+pt2.accountNumber+"\n 에금주 :"+pt2.name+"\n 잔액 : "+pt2.balance+ "원 입니다.\n");
-	}
+		ArrayList<Practice11> bankAccounts = new ArrayList<Practice11>();
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.print(MessagePractice11.MSG_HELLO);
+		boolean judgment = true;
+		while (judgment) {
+			int select = 0;
+			System.out.print(MessagePractice11.MSG_SELECT_MENU);
+			try {
+				select = scanner.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("입력이 잘못되었습니다. 다시입력해주세요");
+				continue;
+			}
+			switch (select) {
+			case CommandPractice11.EXIT:// 종료
+				judgment = false;
+				break;
+			case CommandPractice11.CREATE_ACCOUNT:// 계좌생성
+				System.out.print(MessagePractice11.MSG_CREATE_ACCOUNT_RESIDENTREGISTRATIONNUMBER);
+				String residentRegistrationNumber = scanner.next();
+				System.out.print(MessagePractice11.MSG_CREATE_ACCOUNT_NAME);
+				String name = scanner.next();
+				Practice11  newAccount =new Practice11(residentRegistrationNumber,name);
+				bankAccounts.add(newAccount);
+				System.out.println(String.format(MessagePractice11.MSG_GET_ACCOUNT_NUMBER,newAccount.accountNumber));
+				break;
+			case CommandPractice11.BALANCE_INQUIRY:// 잔액 조회
+				break;
+			case CommandPractice11.BALANCE_DEPOSIT:// 입금
+				break;
+			case CommandPractice11.BALANCE_WITHDRAW:// 출금
+				break;
+
+			}
+		}
+	}
 }
