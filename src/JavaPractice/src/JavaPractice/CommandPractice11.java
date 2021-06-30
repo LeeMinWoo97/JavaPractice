b78d4a61f24973a968edac41e29e6ea4fd427e7b
LeeMinWoo97 <leemin9601@gmail.com>
2021-06-30 21:41:22 +0900

    Java

--- src/JavaPractice/src/JavaPractice/CommandPractice11.java
+++
@@ -1,9 +0,0 @@
-package JavaPractice;
-
-public class CommandPractice11 {
-	public static final int EXIT=0;//종료 swicth case 번호
-	public static final int CREATE_ACCOUNT=1;//계좌생성 swicth case 번호
-	public static final int BALANCE_INQUIRY=2;//잔액 조회 swicth case 번호
-	public static final int BALANCE_DEPOSIT=3;//입금 swicth case 번호
-	public static final int BALANCE_WITHDRAW=4;//출금 swicth case 번호
-}
